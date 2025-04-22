5777e789a10795d9e177712af6277ce025affdb8
GS SHAYKOT <shaykotkhan1916@gmail.com>
2025-04-22 16:04:56 +0600

    cors update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://nexcall-1425e.web.app', 'https://nexcall.up.railway.app'],
+    origin: ['http://localhost:5173', 'https://agora-b3f83.web.app', 'https://agoratest.up.railway.app'],
     credentials: true
 }));
 
